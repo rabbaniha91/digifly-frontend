a5dafa7f5700e26cc6219f658b2fbfbb858dbdfb
rabbaniha <hrabbaniha@gmail.com>
2024-02-03 11:13:46 +0330

    implment breadcramb for products page

---
+++ src/components/products/BreadCrumb.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import classes from "../../assets/styles/responsive.module.css"
+import { Link, useParams } from 'react-router-dom'
+import styles from "./breadCramb.module.css"
+
+const BreadCrumb = () => {
+    const { main_category, sub_category } = useParams()
+
+    const persianCategory = { laptop: "لپ تاپ", pc: "کامپیوتر", mobile: "موبایل", gaming: "کنسول گیمینگ" }
+
+
+    return (
+        <div className={`${classes.responsive} ${styles.container}`}>
+            <Link style={{opacity: "0.5"}} className={styles.link} to={"/"}>فروشگاه اینترنتی دیجی فلای</Link>
+            <span> / </span>
+            <Link className={styles.link} to={`/products/${main_category}`}> {persianCategory[main_category]}</Link>
+            {sub_category && (
+                <>
+                    <span> / </span>
+                    <Link className={styles.link} >{sub_category}</Link>
+                </>
+            )}
+
+        </div>
+    )
+}
+
+export default BreadCrumb
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -3,14 +3,16 @@ import Header from '../components/header'
 import { useParams } from 'react-router-dom'
 import Products from '../components/products/Products'
 import Footer from "../components/footer"
+import BreadCrumb from '../components/products/BreadCrumb'
 
 const ProductsPage = () => {
     const { main_category, sub_category = "" } = useParams()
 
 
     return (
-        <div>
+        <div >
             <Header />
+            <BreadCrumb />
             <Products mainCategory={main_category} subCategory={sub_category} />
             <Footer />
         </div>
